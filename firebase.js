b1c6af7b65f06410a2d7db9fc22db68965b23c77
RonaldErnst <eronald@web.de>
2022-02-03 11:42:43 +0100

    DB Datei umbenannt nach firebase.js

--- firebase.js
+++ firebase.js
@@ -2,6 +2,6 @@
 const firebase = require('firebase-admin');
 const config = require('./config');
 
-const db = firebase.initializeApp(config.firebaseConfig);
+const fb = firebase.initializeApp(config.firebaseConfig);
 
-module.exports = db;
+module.exports = fb;
