dd2f4ce939464a0d10bdaaefbda960b25aa45886
Luca Oberhofer <luca.oberhofer@burda-forward.de>
2022-02-05 13:39:26 +0100

    Background changes

--- server.js
+++ server.js
@@ -7,11 +7,12 @@ const { routes } = require('./routes/test-api');
 
 const app = express();
 
+
 app.use(express.json());
 app.use(cors());
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: false }))
-
+app.use(express.static("public"));
 
 app.use('/api', routes);
 
