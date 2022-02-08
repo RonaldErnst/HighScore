692fa6178cf78ae17f01822e7017e7ea82ba7977
Luca Oberhofer <luca.oberhofer@burda-forward.de>
2022-02-08 12:04:58 +0100

    Added Index FrontEnd

--- pages/index.jsx
+++ pages/index.jsx
@@ -7,7 +7,33 @@ function Home() {
 	return (
 		<>
 			<Header/>
-			Hallo {fireauth.currentUser.displayName}!
+			<div className="mt-16 p-3 w-full md:w-1/2 lg:w-1/3 mx-auto flex flex-col space-y-4">
+
+				<p>Hallo {fireauth.currentUser.displayName}!</p>
+
+				<a>
+					<div className="shadow-md rounded-3xl p-5 border-slate-200 border">
+						<div className="flex flex-row justify-between">
+							<h2>vor 4 Tagen</h2>
+							<span className="border-4 border-emerald-400 py-2 px-3 rounded-3xl shadow">Gras</span>
+						</div>
+						<div className="text-slate-600">Score: 8/10</div>
+					</div>
+				</a>
+
+				<a>
+					<div className="shadow-md rounded-3xl p-5 border-slate-200 border">
+						<div className="flex flex-row justify-between">
+							<h2>vor 10 Tagen</h2>
+							<span className="border-4 border-emerald-400 py-2 px-3 rounded-3xl shadow">Gras</span>
+						</div>
+						<div className="text-slate-600">Score: 5/10</div>
+					</div>
+				</a>
+
+
+			</div>
+
 			<Navigation active="home"></Navigation>
 		</>
 	);
