b76b6a91aa3b020a88c4e3f5209c328f13e4f2d8
Ronald <eronald@web.de>
2022-07-01 13:45:14 +0200

    INIT: Restart

--- firebase.js
+++
@@ -1,9 +0,0 @@
-import { clientCredentials } from './config';
-import { initializeApp } from 'firebase/app';
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-
-const app = initializeApp(clientCredentials);
-
-export const fireauth = getAuth(app);
-export const firestore = getFirestore(app);
